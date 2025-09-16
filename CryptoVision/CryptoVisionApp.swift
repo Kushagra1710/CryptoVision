d6916d0136e86b181309c1bc91c1a07c47aca860
Kushagra Sharma <ks487013@gmail.com>
2025-09-16 20:16:49 +0530

    Initial Commit

---
+++ CryptoVision/CryptoVisionApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoVisionApp.swift
+//  CryptoVision
+//
+//  Created by Kushagra Sharma on 16/09/25.
+//
+
+import SwiftUI
+
+@main
+struct CryptoVisionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
